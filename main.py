efdcd95b0720d7d651272925c5a38fddb1959766
g-i-t-lex <as.arge1997@gmail.com>
2024-02-12 14:35:26 -0300

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#prompt de comando
+
+#git bash
+#cd = change directory = mudar diretorio
+#ls = vai carregar as pastas/docs
+#git --version
+#git init = criar repositorio
+#ls -all //(mostra ate as pastas ocultas)
+
+
+print( " Use - python main.py pra executar via prompt/bash")
+
